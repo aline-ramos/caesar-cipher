9d7ecb58a21757fabe6ea2583546e8b6d104cfd9
Hirao <gigihirao@gmail.com>
2019-02-01 17:27:57 -0200

    Projeto Cifra

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,88 @@
-window.cipher = {
-  // ... 
-};
+document.getElementById('botcifra').addEventListener('click', cifrar); 
+document.getElementById('botdecifra').addEventListener('click', decifrar); 
+
+  function cifrar (){
+    let n = parseInt(document.getElementById('desloc').value);    
+    let mensag = document.getElementById('cripto').value;
+    let result = cifTexto(mensag,n);
+
+
+    document.getElementById('cript').innerHTML = "<div id=cript> Mensagem <br><span >"+
+    "<input type=text name=crypter id= result size =40 value= '"+result+"'> <br>"
+  }
+
+  function decifrar (){
+    let n= parseInt(document.getElementById('desloc').value);
+    let mensag = document.getElementById('cripto').value;
+    let result = decifTexto(mensag,n);
+        
+
+    document.getElementById('cript').innerHTML = "<div id=cript> Mensagem <br><span >"+
+    "<input type=text name=crypter id= result size =40 value= '"+result+"'> <br>"
+
+    return (n,mensag);  
+  
+  }
+    
+    
+  
+
+  function cifTexto(texto, number){
+    let textUm="";
+    let num;
+        
+    for (let i = 0; i < texto.length; i++) {
+     num = texto.charCodeAt([i]);
+      
+     if(num >= 65 && num <=90){
+      num = ((num - 65 + number)%26)+65;
+      textUm = textUm.concat(String.fromCharCode(num));
+      
+     }
+
+     else if (num >= 97 && num <=122){
+      num = ((num - 97 + number)%26)+97;
+      textUm = textUm.concat(String.fromCharCode(num));
+      
+     }
+     else {
+       textUm = textUm.concat(String.fromCharCode(num));
+       
+      }
+
+    }
+    
+   return textUm;
+  }
+
+  
+  function decifTexto(texto, number){
+    let textUm="";
+    let num;
+        
+    for (let i = 0; i < texto.length; i++) {
+     num = texto.charCodeAt([i]);
+      
+     if(num >= 65 && num <=90){
+      num = ((num - 90 - number)%26)+90;
+      textUm = textUm.concat(String.fromCharCode(num));
+      
+     }
+
+     else if (num >= 97 && num <=122){
+      num = ((num - 122 - number)%26)+122;
+      textUm = textUm.concat(String.fromCharCode(num));
+      
+     
+     }
+     else {
+       textUm = textUm.concat(String.fromCharCode(num));
+      
+      }
+
+    }
+   
+   return textUm;
+  }
+
+  
---
+++ src/tests.js
@@ -0,0 +1,54 @@
+if (cifTexto("abc", 1) === "bcd"){
+    console.log("Ok");
+    } else {
+    console.log("erro");
+    }
+    
+    if (cifTexto("EFG", 3) === "HIJ"){
+        console.log("Ok");
+    } else {
+        console.log("erro");
+    }
+    
+    if (cifTexto("AbC", 10) === "KlM"){
+        console.log("Ok");
+    } else {
+        console.log("erro");
+    }
+    
+    if (cifTexto("ZAz", 15) === "OPo"){
+        console.log("Ok");
+    } else {
+        console.log("erro");
+    }
+    
+    if (decifTexto("QrS", 10) === "GhI"){
+        console.log("Ok");
+    } else {
+        console.log("erro");
+    }
+    
+    if (decifTexto("Klk", 15) === "Vwv"){
+        console.log("Ok");
+    } else {
+        console.log("erro");
+    }
+    
+    if (cifTexto("Laboratória", 5) === "Qfgtwfyównf"){
+        console.log("Ok");
+    } else {
+        console.log("erro");
+    }
+    
+    if (decifTexto("C if Czcf", 5) === "X da Xuxa"){
+        console.log("Ok");
+    } else {
+        console.log("erro");
+    }
+    
+    if (decifTexto("Dofcuhu, Ṕufigu, Luzuyf", 20) === "Juliana, Ṕaloma, Rafael"){
+        console.log("Ok");
+    } else {
+        console.log("erro");
+    }
+    
